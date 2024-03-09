6d50cd245862490164a2f755ef2cb6172739550d
Juliane Maran <julianemaran@gmail.com>
2024-03-09 12:53:03 -0300

    feat: welcome

---
+++ eazybytes/src/main/java/com/springsecurity/eazybytes/controller/WelcomeController.java
@@ -0,0 +1,16 @@
+package com.springsecurity.eazybytes.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class WelcomeController {
+
+	@GetMapping("/welcome")
+	public String sayWelcome(){
+		return "Welcome to Spring Application with Security";
+	}
+
+}
