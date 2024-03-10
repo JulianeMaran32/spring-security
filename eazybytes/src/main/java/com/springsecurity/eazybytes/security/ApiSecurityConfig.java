c50eed7488768ad9362fa3ebde7fa2ec2371e15e
Juliane Maran <julianemaran@gmail.com>
2024-03-09 21:31:00 -0300

    feat: Configuration to permit all the requests

---
+++ eazybytes/src/main/java/com/springsecurity/eazybytes/security/ApiSecurityConfig.java
@@ -0,0 +1,26 @@
+package com.springsecurity.eazybytes.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class ApiSecurityConfig {
+
+	/**
+	 * Configuration to permit all the requests
+	 */
+	@Bean
+	SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+
+		http
+				.authorizeHttpRequests(request -> request.anyRequest().permitAll())
+				.formLogin(Customizer.withDefaults())
+				.httpBasic(Customizer.withDefaults());
+		return http.build();
+
+	}
+
+}
