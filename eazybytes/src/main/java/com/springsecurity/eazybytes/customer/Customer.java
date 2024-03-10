d4b55064a982b9c950c254031715de58e42f630c
Juliane Maran <julianemaran@gmail.com>
2024-03-09 22:15:17 -0300

    feat: add customer

---
+++ eazybytes/src/main/java/com/springsecurity/eazybytes/customer/Customer.java
@@ -0,0 +1,27 @@
+package com.springsecurity.eazybytes.customer;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "customers")
+public class Customer {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Email
+	private String email;
+
+	private String pwd;
+
+	private String role;
+
+}
