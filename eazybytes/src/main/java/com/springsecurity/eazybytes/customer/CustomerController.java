32804b8c9da36c76c57b3427afa09391e418abc7
Juliane Maran <julianemaran@gmail.com>
2024-03-09 22:23:32 -0300

    feat: adiciona repositorio e controller para customer

---
+++ eazybytes/src/main/java/com/springsecurity/eazybytes/customer/CustomerController.java
@@ -0,0 +1,44 @@
+package com.springsecurity.eazybytes.customer;
+
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/customers")
+@AllArgsConstructor
+public class CustomerController {
+
+	private final CustomerRepository customerRepository;
+
+	@PostMapping("/register")
+	public ResponseEntity<String> registerCustomer(@RequestBody Customer customer) {
+
+		Customer savedCustomer = null;
+		ResponseEntity response = null;
+
+		try {
+
+			savedCustomer = customerRepository.save(customer);
+
+			if (savedCustomer.getId() > 0) {
+				response = ResponseEntity
+						.status(HttpStatus.CREATED)
+						.body("Given user details are successfully registered");
+			}
+
+		} catch (Exception ex) {
+			response = ResponseEntity
+					.status(HttpStatus.INTERNAL_SERVER_ERROR)
+					.body("An exception occured due to " + ex.getMessage());
+		}
+
+		return response;
+
+	}
+
+}
